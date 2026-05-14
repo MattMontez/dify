4450a3183784f2a23fd075df65401351bbbbde4a
fatelei <fatelei@gmail.com>
2026-05-14 11:18:40 +0800

    chore: add miss migration file

---
+++ api/migrations/versions/2026_05_14_1230-5a9d4e7c2b11_merge_all_current_heads.py
@@ -0,0 +1,22 @@
+"""merge all current heads
+
+Revision ID: 5a9d4e7c2b11
+Revises: 4c60d8d3ee74, 1f6e6cb0f0ac
+Create Date: 2026-05-14 12:30:00.000000
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "5a9d4e7c2b11"
+down_revision = ("4c60d8d3ee74", "1f6e6cb0f0ac")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
